13e4dfcc56140bcbbe4cba135198c268adf40f06
vagrant <vagrant@localhost.localdomain>
2019-01-30 04:48:17 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Tyler'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
