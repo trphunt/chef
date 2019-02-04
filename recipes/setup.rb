b233928b5f7cd45b5d6a6427cfe3bc73b1089157
trphunt <trphunt@tylerhunt.solutions>
2019-02-04 03:47:03 +0000

    refactored motd file with a template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -17,15 +17,8 @@ end
 node['ipaddress']
 node['memory']['total']
 
-file '/etc/motd' do
-  content "This server is the property of Tyler
-  HOSTNAME: #{node['hostname']}
-  IPADDRESS: #{node['ipaddress']}
-  CPU: #{node['cpu']['0']['mhz']}
-  MEMORY: #{node['memory']['total']}
-"
-  owner 'root'
-  group 'root'
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
 end
 
